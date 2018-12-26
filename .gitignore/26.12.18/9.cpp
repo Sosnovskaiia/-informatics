0100a69aa3295fd58e96a87ebca10c79e2ac462b
Sosnovskaiia <44139937+Sosnovskaiia@users.noreply.github.com>
2018-12-26 23:42:46 +0300

    Create 9.cpp

---
+++ .gitignore/26.12.18/9.cpp
@@ -0,0 +1,37 @@
+/*Напишите программу, находящую все простые числа Софи Жермен не превосходящие n. Программа должна работать не более чем за 
+O(n*log log n) шагов.*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+bool huh(int n)
+{
+    int i=2, k=0;
+    k=sqrt(n);
+    while (((n% i)!=0) && (i<=k))
+    {
+        i++;
+       
+    }
+     return i>k;
+}
+    int main ()
+    {
+        int i,n,m=0;
+        
+        cin >> n;
+        
+        for (i=0; i<n;i++)
+        {
+            if (huh(i) && huh(i*2+1) )
+            {
+                cout << i<<' ';
+                m=1;
+            }
+        }
+        if (m==0 )
+            cout <<m;
+        
+        return 0;
+    }
